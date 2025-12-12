eb2ddf9cd02f8d47cc10d127744d7c81beea75d2
Mallikarjuna Kadiwal <143894803+mallikarjunaKadiwal@users.noreply.github.com>
2025-12-12 18:12:45 +0530

    Cleanup
    Removed commented steps for clarity.

--- project.js
+++ project.js
@@ -1,11 +1,3 @@
-// 1. Deposit some money
-// 2. Determine number of lines to bet on
-// 3. Collect a bet amount
-// 4. Spin the slot machine
-// 5. check if the user won
-// 6. give the user their winnings
-// 7. play again
-
 const prompt = require("prompt-sync")();
 
 const ROWS = 3;
